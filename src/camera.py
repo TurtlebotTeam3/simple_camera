c73a967444b7fefa1e0c9507b20e8d5adcd4802d
jan <fleisch.ja@gmail.com>
2020-01-16 23:07:43 +0100

    changed method for blob detection and centroid calculation

--- src/camera.py
+++ src/camera.py
@@ -12,15 +12,14 @@ class Camera():
     def __init__(self):
         rospy.init_node('CameraNode')
         rospy.loginfo('Cam test node started')
-        self.COLOR = np.array([110,50,50])
-
-        self.center = 0
-        self.blob_x = 0
-        self.blob_y = 0
 
         #cameraNode parameters
+        self.COLOR = np.array([110,50,50])
+        self.minAreaSize = 600
+        self.blob_x = 0
+        self.blob_y = 0
         self.blob_detected = False
-        self.minBlobWidth = 80
+
         #subscribe to camera image
         self.bridge = cv_bridge.CvBridge()
         self.sub = rospy.Subscriber('/raspicam_node/image/compressed', sensor_msgs.msg.CompressedImage, self.run)
@@ -31,83 +30,66 @@ class Camera():
         self.pub.publish(False)
         rospy.spin() 
         
-
+        
     def run(self, image):
         #get frame from robo
         frame = self.bridge.compressed_imgmsg_to_cv2(image, desired_encoding='bgr8')
-        #filter image and detect blob
-        #cv2.imshow('img1', frame) 
-        filteretImage = self.filterImage(frame)
+        mask = self.calculateMask(frame)
 
-        #tempBlopDetected = self.detectBlob(filteretImage, len(frame), len(frame[0]), [-1] )
-        #print and show image
-        #print "blob_detected: " + str(self.blob_detected) + " x: " + str(self.blob_x) + " y: " + str(self.blob_y) + " Center: " + str(self.center)
-        #cv2.circle(filteretImage, (self.blob_y, self.blob_x), self.minBlobWidth, self.COLOR, thickness=5, lineType=8, shift=0)
-        cv2.imshow('img2', filteretImage) 
+        (self.blob_x, self.blob_y) = self.find_center(mask, self.minAreaSize)
+
         #publish
         #if not rospy.is_shutdown() and self.blob_detected != tempBlopDetected:
         #    self.pub.publish(tempBlopDetected)
         #    self.blob_detected = tempBlopDetected
+
+        #show images
+        cv2.imshow('normal_image', mask) 
+        cv2.circle(mask, (self.blob_x, self.blob_y), 20, self.COLOR, thickness=5, lineType=8, shift=0)
         cv2.waitKey(10)
 
-    def filterImage(self, frame):
-        #convert to hsv image
-        hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV) 
-        #red
-            #lower mask red
-            #lower_red = np.array([0, 50, 50])
-            #upper_red = np.array([10, 255, 255])
-            #mask0 = cv2.inRange(hsv, lower_red, upper_red)
-            #upper mask red
-            #lower_red = np.array([170, 50, 50])
-            #upper_red = np.array([180, 255, 255])
-            #mask1 = cv2.inRange(hsv, lower_red, upper_red)
-            #join my masks for upper and lower red
-            #mask = mask0 + mask1
-        #green
-        lowerBound = np.array([50, 50, 10],np.uint8)
-        upperBound = np.array([90, 255, 255],np.uint8)
-       
+    def calculateMask(self, frame):
+        im = cv2.blur(frame, (3, 3))
+
+        # convert to hsv image
+        hsv = cv2.cvtColor(im, cv2.COLOR_BGR2HSV)
+
+        # green bounderies
+        lowerBound = np.array([50, 50, 10], np.uint8)
+        upperBound = np.array([90, 255, 255], np.uint8)
+
         mask = cv2.inRange(hsv, lowerBound, upperBound)
-        #cv2.imwrite('img.png',frame)
-        #cv2.imshow('img3', mask)
-        
-        #bitwise-not mask and original image
-        res = cv2.bitwise_not(mask)
 
-        return res 
+        return mask 
+    
+    def find_center(self, color, minArea):
+        contours, hierarchy = cv2.findContours(color, cv2.RETR_LIST, cv2.CHAIN_APPROX_SIMPLE)
 
-    def detectBlob(self, image, resolX, resolY, xCenter):
-        newimage = image
-        minBlobWidth = self.minBlobWidth
-        xStart = -1
-        for y in range(resolY):
-            blobwidth = 0
-            for x in range(resolX):
-                pixel = newimage[x, y]
-                #print pixel
-                if pixel == 0:  
-                    blobwidth += 1
-                    if  blobwidth == 1:
-                        xStart = x
-                else:
-                    if blobwidth >= minBlobWidth:
-                        xCenter[0] = xStart + blobwidth/2
-                        #print('blob detected at: ', xStart, y, ' with center at: ', xCenter[0])
-                        self.blob_x = xStart
-                        self.blob_y = y
-                        self.center = xCenter[0]
-                        return True
-                    elif blobwidth > 0:
-                        blobwidth = 0
-            if blobwidth >= minBlobWidth:
-                xCenter[0] = xStart + blobwidth/2
-                self.blob_x = xStart
-                self.blob_y = y
-                self.center = xCenter[0]
-                #print('blob detected at: ', xStart, y, ' with center at: ', xCenter[0])
-                return True
-        return False
+        if len(contours) > 0:
+            # find biggest blob
+            max_area = 0
+            for contour in contours:
+                area = cv2.contourArea(contour)
+                if area > max_area:
+                    max_area = area
+                    best_blob = contour
+            if max_area > minArea:
+                # find centroid
+                M = cv2.moments(best_blob)
+                cx = int(M['m10'] / M['m00'])
+                cy = int(M['m01'] / M['m00'])
+                cv2.circle(im, (cx, cy), 3, 255, -1)
+                #(xcenter, ycenter), (MA, ma), angle = cv2.fitEllipse(best_blob)
+                print "maxarea: " + str(max_area)
+                print "position: " + str((cx, cy))
+                return int(cx), int(cy)
+            else:
+                print "too small"
+                print "maxarea: " + str(max_area)
+                return 0, 0
+        else:
+            print "not found!"
+            return 0, 0
 
 
 if __name__ == '__main__':
