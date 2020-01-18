3fd0d499fa6124d770dbe628d0e0e8f823e49f5a
jan <fleisch.ja@gmail.com>
2020-01-18 15:13:52 +0100

    added costume msg and cleand up some code parts

--- src/camera.py
+++ src/camera.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 import rospy
-from std_msgs.msg import Bool
+from simple_camera.msg import Blob
 import sensor_msgs.msg
 import numpy as np
 import cv2
@@ -16,55 +16,73 @@ class Camera():
         #cameraNode parameters
         self.COLOR = np.array([110,50,50])
         self.minAreaSize = 600
+        #blob parameters
         self.blob_x = 0
         self.blob_y = 0
-        self.blob_detected = False
+        self.blob_in_front = False
 
         #subscribe to camera image
         self.bridge = cv_bridge.CvBridge()
         self.sub = rospy.Subscriber('/raspicam_node/image/compressed', sensor_msgs.msg.CompressedImage, self.run)
         #publisher blob detected
-        self.pub = rospy.Publisher('blob_detected', Bool, queue_size=10)
+        self.pub_blob = rospy.Publisher("blob", Blob, queue_size=10)
         self.rate = rospy.Rate(2)
-        #publish false on init
-        self.pub.publish(False)
+        #publish false on init 
+        self.blob_msg = Blob()
+        self.blob_msg.blob_detected = self.blob_in_front
+        self.blob_msg.blob_x = self.blob_x
+        self.blob_msg.blob_y = self.blob_y
+        self.pub_blob.publish(self.blob_msg)
+
         rospy.spin() 
         
         
     def run(self, image):
         #get frame from robo
-        frame = self.bridge.compressed_imgmsg_to_cv2(image, desired_encoding='bgr8')
-        mask = self.calculateMask(frame)
-
+        frame = self.bridge.compressed_imgmsg_to_cv2(image, desired_encoding='bgr8')  
+        #cut the top of the frame to only see 45 cm
+        crop_frame = frame[250:720, 0:1280]
+        #calculate hsv mask
+        mask = self.calculateMask(crop_frame)
+        #detect blob if x y == 0 no blob detected
         (self.blob_x, self.blob_y) = self.find_center(mask, self.minAreaSize)
-
         #publish
-        #if not rospy.is_shutdown() and self.blob_detected != tempBlopDetected:
-        #    self.pub.publish(tempBlopDetected)
-        #    self.blob_detected = tempBlopDetected
+        if not rospy.is_shutdown() and self.blob_x != 0 and self.blob_y != 0:
+            self.blob_in_front = True
+            self.blob_msg.blob_detected = self.blob_in_front
+            self.blob_msg.blob_x = self.blob_x
+            self.blob_msg.blob_y = self.blob_y
+            self.pub_blob.publish(self.blob_msg)
+        else:
+            self.blob_in_front = False
+            self.blob_msg.blob_detected = self.blob_in_front
+            self.blob_msg.blob_x = self.blob_x
+            self.blob_msg.blob_y = self.blob_y
+            self.pub_blob.publish(self.blob_msg)
+        #show image with centroid
+        self.showImage("img1", mask, True)
+        
 
+    def showImage(self, name, img, centroid):
         #show images
-        cv2.imshow('normal_image', mask) 
-        cv2.circle(mask, (self.blob_x, self.blob_y), 20, self.COLOR, thickness=5, lineType=8, shift=0)
-        cv2.waitKey(10)
+        if centroid == True:
+            cv2.circle(img, (self.blob_x, self.blob_y), 20, self.COLOR, thickness=5, lineType=8, shift=0)
+        cv2.imshow(name, img) 
+        cv2.waitKey(1)
 
     def calculateMask(self, frame):
         im = cv2.blur(frame, (3, 3))
-
         # convert to hsv image
         hsv = cv2.cvtColor(im, cv2.COLOR_BGR2HSV)
-
         # green bounderies
         lowerBound = np.array([50, 50, 10], np.uint8)
         upperBound = np.array([90, 255, 255], np.uint8)
-
+        # mask in Range of lowerBlue and upperBlue
         mask = cv2.inRange(hsv, lowerBound, upperBound)
-
         return mask 
     
     def find_center(self, color, minArea):
-        contours, hierarchy = cv2.findContours(color, cv2.RETR_LIST, cv2.CHAIN_APPROX_SIMPLE)
-
+        _, contours, _ = cv2.findContours(color, cv2.RETR_LIST, cv2.CHAIN_APPROX_SIMPLE)
         if len(contours) > 0:
             # find biggest blob
             max_area = 0
@@ -78,7 +96,7 @@ class Camera():
                 M = cv2.moments(best_blob)
                 cx = int(M['m10'] / M['m00'])
                 cy = int(M['m01'] / M['m00'])
-                cv2.circle(im, (cx, cy), 3, 255, -1)
+                #cv2.circle(im, (cx, cy), 3, 255, -1)
                 #(xcenter, ycenter), (MA, ma), angle = cv2.fitEllipse(best_blob)
                 print "maxarea: " + str(max_area)
                 print "position: " + str((cx, cy))
@@ -90,7 +108,6 @@ class Camera():
         else:
             print "not found!"
             return 0, 0
-
 
 if __name__ == '__main__':
 	Camera()
