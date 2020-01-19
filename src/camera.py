00b06464a332a8348fa39963133bd04c50f07c54
jan <fleisch.ja@gmail.com>
2020-01-19 17:53:04 +0100

    parameter fine tuning

--- src/camera.py
+++ src/camera.py
@@ -15,7 +15,7 @@ class Camera():
 
         #cameraNode parameters
         self.COLOR = np.array([110,50,50])
-        self.minAreaSize = 600
+        self.minAreaSize = 2000
         #blob parameters
         self.blob_x = 0
         self.blob_y = 0
@@ -23,10 +23,10 @@ class Camera():
 
         #subscribe to camera image
         self.bridge = cv_bridge.CvBridge()
-        self.sub = rospy.Subscriber('/raspicam_node/image/compressed', sensor_msgs.msg.CompressedImage, self.run)
+        self.sub = rospy.Subscriber('/raspicam_node/image/compressed', sensor_msgs.msg.CompressedImage, self._run)
         #publisher blob detected
         self.pub_blob = rospy.Publisher("blob", Blob, queue_size=10)
-        self.rate = rospy.Rate(2)
+        self.rate = rospy.Rate(20)
         #publish false on init 
         self.blob_msg = Blob()
         self.blob_msg.blob_detected = self.blob_in_front
@@ -37,15 +37,16 @@ class Camera():
         rospy.spin() 
         
         
-    def run(self, image):
+    def _run(self, image):
         #get frame from robo
         frame = self.bridge.compressed_imgmsg_to_cv2(image, desired_encoding='bgr8')  
+        #self._showImage("img2", frame, False)
         #cut the top of the frame to only see 45 cm
-        crop_frame = frame[250:720, 0:1280]
+        frame = frame[0:720, 0:1280]
         #calculate hsv mask
-        mask = self.calculateMask(crop_frame)
+        mask = self._calculateMask(frame)
         #detect blob if x y == 0 no blob detected
-        (self.blob_x, self.blob_y) = self.find_center(mask, self.minAreaSize)
+        (self.blob_x, self.blob_y) = self._find_center(mask, self.minAreaSize)
         #publish
         if not rospy.is_shutdown() and self.blob_x != 0 and self.blob_y != 0:
             self.blob_in_front = True
@@ -60,17 +61,17 @@ class Camera():
             self.blob_msg.blob_y = self.blob_y
             self.pub_blob.publish(self.blob_msg)
         #show image with centroid
-        self.showImage("img1", mask, True)
+        self._showImage("img1", mask, True)
         
 
-    def showImage(self, name, img, centroid):
+    def _showImage(self, name, img, centroid):
         #show images
         if centroid == True:
             cv2.circle(img, (self.blob_x, self.blob_y), 20, self.COLOR, thickness=5, lineType=8, shift=0)
         cv2.imshow(name, img) 
         cv2.waitKey(1)
 
-    def calculateMask(self, frame):
+    def _calculateMask(self, frame):
         im = cv2.blur(frame, (3, 3))
         # convert to hsv image
         hsv = cv2.cvtColor(im, cv2.COLOR_BGR2HSV)
@@ -81,7 +82,7 @@ class Camera():
         mask = cv2.inRange(hsv, lowerBound, upperBound)
         return mask 
     
-    def find_center(self, color, minArea):
+    def _find_center(self, color, minArea):
         _, contours, _ = cv2.findContours(color, cv2.RETR_LIST, cv2.CHAIN_APPROX_SIMPLE)
         if len(contours) > 0:
             # find biggest blob
@@ -106,10 +107,13 @@ class Camera():
                 print "maxarea: " + str(max_area)
                 return 0, 0
         else:
-            print "not found!"
+            #print "not found!"
             return 0, 0
 
 if __name__ == '__main__':
-	Camera()
+    try:
+        camera=Camera()
+    except rospy.ROSInterruptException:
+        pass
 
 
