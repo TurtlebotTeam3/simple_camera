b249e40d8a90956ef5e4e424dbb8a67dec96c4c5
jan <fleisch.ja@gmail.com>
2020-01-25 10:20:24 +0100

    call service tag mangaer

--- src/camera.py
+++ src/camera.py
@@ -10,6 +10,10 @@ from tf.transformations import euler_from_quaternion
 from geometry_msgs.msg import PointStamped
 import tf
 from geometry_msgs.msg._Pose import Pose
+from tag_manager.srv import CheckTagKnown
+from tag_manager.srv import AddTag
+from nav_msgs.msg._OccupancyGrid import OccupancyGrid
+import math
 
 class Camera(): 
 
@@ -17,14 +21,20 @@ class Camera():
         rospy.init_node('CameraNode')
         rospy.loginfo('Cam test node started')
 
+        self.mapSub = rospy.Subscriber('/map', OccupancyGrid, self._map_callback)
+
         #cameraNode parameters
         self.COLOR = np.array([110,50,50])
-        self.minAreaSize = 200
+        self.minAreaSize = 600
         #blob parameters
         self.blob_x = 0
         self.blob_y = 0
         self.blob_in_front = False
-
+        self.call_service = False
+        self.map_resolution = 0
+        self.map_offset_x = 0
+        self.map_offset_y = 0
+        self.received_map = False
         #subscribe to camera image
         self.bridge = cv_bridge.CvBridge()
         self.sub = rospy.Subscriber('/raspicam_node/image/compressed', sensor_msgs.msg.CompressedImage, self._run)
@@ -34,9 +44,14 @@ class Camera():
         #publish false on init 
         self.blob_msg = Blob()
         self.blob_msg.blob_detected = self.blob_in_front
-        self.blob_msg.blob_x = self.blob_x
-        self.blob_msg.blob_y = self.blob_y
+        self.blob_msg.map_x = 0
+        self.blob_msg.map_y = 0
         self.pub_blob.publish(self.blob_msg)
+        #service tag_manager
+        rospy.wait_for_service('check_tag_known')
+        self.tag_manager_check = rospy.ServiceProxy('check_tag_known', CheckTagKnown)
+        rospy.wait_for_service('add_tag')
+        self.tag_manager_add = rospy.ServiceProxy('add_tag', AddTag)
 
         self.pose = Pose()
         self.pose_subscriber = rospy.Subscriber('/simple_odom_pose', Pose, self._update_pose)
@@ -60,7 +75,13 @@ class Camera():
 		
 		self.pose.position.x = round(data.position.x, 4)
 		self.pose.position.y = round(data.position.y, 4)
-		self.robot_yaw = self._robot_angle()   
+		self.robot_yaw = self._robot_angle()  
+
+    def _map_callback(self, data):
+        self.map_resolution = data.info.resolution
+        self.map_offset_x = data.info.origin.position.x
+        self.map_offset_y = data.info.origin.position.y
+        self.received_map = True
 
     def _robot_angle(self):
 		orientation_q = self.pose.orientation
@@ -69,37 +90,47 @@ class Camera():
 		return yaw  
         
     def _run(self, image):
-        #get frame from robo
-        frame = self.bridge.compressed_imgmsg_to_cv2(image, desired_encoding='bgr8') 
-        #TODO DELETE
-        #cv2.imwrite('03-03.jpg',frame)
-        #raw_input("stop")
+        if self.received_map == True:
+            #get frame from robo
+            frame = self.bridge.compressed_imgmsg_to_cv2(image, desired_encoding='bgr8') 
+            #TODO DELETE
+            #cv2.imwrite('03-03.jpg',frame)
+            #raw_input("stop")
 
-        #self._showImage("img2", frame, False)
-        #cut the top of the frame to only see 45 cm
-        frame = frame[0:720, 0:1280]
-        #calculate hsv mask
-        mask = self._calculateMask(frame)
-        #detect blob if x y == 0 no blob detected
-        (self.blob_x, self.blob_y) = self._find_center(mask, self.minAreaSize)
-        #publish
-        if not rospy.is_shutdown() and self.blob_x != 0 and self.blob_y != 0:
-            self.blob_in_front = True
-            self.blob_msg.blob_detected = self.blob_in_front
-            self.blob_msg.blob_x = self.blob_x
-            self.blob_msg.blob_y = self.blob_y
-            self.pub_blob.publish(self.blob_msg)
-            self._calculatePositionOfTag(self.blob_x, self.blob_y)
-        else:
-            self.blob_in_front = False
-            self.blob_msg.blob_detected = self.blob_in_front
-            self.blob_msg.blob_x = self.blob_x
-            self.blob_msg.blob_y = self.blob_y
-            self.pub_blob.publish(self.blob_msg)
-        #show image with centroid
-        self._showImage("img1", mask, True)
+            #self._showImage("img2", frame, False)
+            #cut the top of the frame to only see 45 cm
+            frame = frame[0:720, 0:1280]
+            #calculate hsv mask
+            mask = self._calculateMask(frame)
+            #detect blob if x y == 0 no blob detected
+            (self.blob_x, self.blob_y) = self._find_center(mask, self.minAreaSize)
+            #publish
+            if not rospy.is_shutdown() and self.blob_x != 0 and self.blob_y != 0:
+                self.blob_in_front = True
+                self.blob_msg.blob_detected = self.blob_in_front
+                x_gobal, y_global = self._calculateMapPosOfTag(self.blob_x, self.blob_y)
+                x_in_map = int(math.floor((x_gobal - self.map_offset_x)/self.map_resolution))
+                y_in_map = int(math.floor((y_global - self.map_offset_y)/self.map_resolution))
+                self.blob_msg.map_x = x_in_map
+                self.blob_msg.map_y = y_in_map
+                self.pub_blob.publish(self.blob_msg)
+                if self.call_service == False:
+                    self.call_service = True
+                    check_service_response = self.tag_manager_check(x_in_map,y_in_map)
+                    if check_service_response.tagKnown.data == False:
+                        add_service_response = self.tag_manager_add(x_in_map,y_in_map)
+                        print add_service_response
+                        print "Added New Tag"
+            else:
+                self.call_service = False
+                self.blob_in_front = False
+                self.blob_msg.blob_detected = self.blob_in_front
+                self.blob_msg.map_x = 0
+                self.blob_msg.map_y = 0
+                self.pub_blob.publish(self.blob_msg)
+            #show image with centroid
+            self._showImage("img1", mask, True)
         
-
     def _showImage(self, name, img, centroid):
         #show images
         if centroid == True:
@@ -146,7 +177,7 @@ class Camera():
             #print "not found!"
             return 0, 0
     
-    def _calculatePositionOfTag(self, x, y):
+    def _calculateMapPosOfTag(self, x, y):
         H = np.array([[0.230489985832561, 3.57275008588951e-05, -146.602459530058],
                       [0.00589738679860119, 0.120006465885370, 257.753591375280],
                       [2.75956230239940e-05 , 0.0172475696150036, 1.0]])
@@ -158,12 +189,6 @@ class Camera():
 
         _, _, phi = euler_from_quaternion([self.pose.orientation.x, self.pose.orientation.y, self.pose.orientation.z, self.pose.orientation.w])
 
-        print phi
-        print self.pose.position.x
-        print self.pose.position.y
-        print self.pose.orientation.z
-        print self.pose.orientation.w
-
         T = np.asarray(
             [
                 [np.cos(phi), -np.sin(phi), self.pose.position.x],
@@ -172,19 +197,7 @@ class Camera():
             ]
         )
 
-        x, y, z = np.matmul(T, np.asarray([(x/100), (y/100), 1]))
-
-        print "tag at x: " + str(x)
-        print "tag at y: " + str(y)
-        print "tag at z: " + str(z)
-
-        #pt = PointStamped()
-        #pt.header.frame_id = "base_link"
-        #pt.point.x = x
-        #pt.point.y = y
-        #pt.point.z = 1
-
-        #self.marker_publisher.publish(pt)
+        x, y, _ = np.matmul(T, np.asarray([(x/100), (y/100), 1]))
 
         return [x, y]
 
