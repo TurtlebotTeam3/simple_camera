5e27a65960040a9667ecbc2833b8719bbcce9513
jan <fleisch.ja@gmail.com>
2020-01-30 14:49:28 +0100

    srv to stop blob detection check tag known

--- src/camera.py
+++ src/camera.py
@@ -14,6 +14,8 @@ from tag_manager.srv import CheckTagKnown
 from nav_msgs.msg._OccupancyGrid import OccupancyGrid
 from std_msgs.msg._Bool import Bool
 from simple_camera.msg import Blob
+from simple_camera.srv import EnableBlobDetection, EnableBlobDetectionResponse
+from simple_camera.srv import EnableTagKnownCheck, EnableTagKnownCheckResponse
 
 class Camera(): 
 
@@ -21,12 +23,8 @@ class Camera():
         rospy.init_node('CameraNode')
         rospy.loginfo('Cam test node started')
 
-        self.mapSub = rospy.Subscriber('/map', OccupancyGrid, self._map_callback)
-
-        #cameraNode parameters
         self.COLOR = np.array([110,50,50])
-        self.minAreaSize = 1600#6500
-        #blob parameters
+        self.minAreaSize = 1600 #6500
         self.blob_x = 0
         self.blob_y = 0
         self.blob_in_front = False
@@ -34,39 +32,58 @@ class Camera():
         self.map_offset_x = 0
         self.map_offset_y = 0
         self.received_map = False
-        #subscribe to camera image
         self.bridge = cv_bridge.CvBridge()
-        self.sub = rospy.Subscriber('/raspicam_node/image/compressed', sensor_msgs.msg.CompressedImage, self._run)
-        #publisher blob detected
-        self.pub_blob = rospy.Publisher("blob", Blob, queue_size=10)
-        #self.rate = rospy.Rate(20)
+        self.pose = Pose()
+        self.do_blob_detection = True
+        self.do_tag_known_check = True
+
+        print("--- publisher ---")
+        # --- Publishers ---
+        self.blob_publisher = rospy.Publisher("blob", Blob, queue_size=10)
+        self.stop_move_to_goal_publisher = rospy.Publisher('move_to_goal/pause_action', Bool, queue_size=1)
+        self.move_to_tag_publisher = rospy.Publisher('move_to_tag_start_driving', Bool, queue_size=10)
+
+        print("--- subscriber ---")
+        # --- Subscribers ---
+        self.map_subscriber = rospy.Subscriber('/map', OccupancyGrid, self._map_callback)
+        self.camera_subscriber = rospy.Subscriber('/raspicam_node/image/compressed', sensor_msgs.msg.CompressedImage, self._run)
+        self.pose_subscriber = rospy.Subscriber('/simple_odom_pose', Pose, self._update_pose)
+        self.move_to_goal_is_paused_subscriber = rospy.Subscriber('/move_to_goal/paused', Bool, self._move_to_tag)
+
+        print("--- service wait ---")
+        # --- Service wait ---
+        print("1")
+        rospy.wait_for_service('check_tag_known')
+
+        print("--- services ---")
+        self.tag_manager_check_service = rospy.ServiceProxy('check_tag_known', CheckTagKnown)
+
         #publish false on init 
         self.blob_msg = Blob()
         self.blob_msg.blob_detected = False
         self.blob_msg.blob_x = 0
         self.blob_msg.blob_y = 0
-        self.pub_blob.publish(self.blob_msg)
+        self.blob_publisher.publish(self.blob_msg)
 
-        #service tag_manager
-        print("--- wait for check_tag_known service")
-        rospy.wait_for_service('check_tag_known')
-        self.tag_manager_check = rospy.ServiceProxy('check_tag_known', CheckTagKnown)
-        print("--- check_tag_known service ready")
-
-        #simpel odom
-        self.pose = Pose()
-        self.pose_subscriber = rospy.Subscriber('/simple_odom_pose', Pose, self._update_pose)
-
-        #stop move to goal
-        self.stop_move_to_goal_publisher = rospy.Publisher('move_to_goal/pause_action', Bool, queue_size=1)
-
-        #move_to_goal is paused
-        self.move_to_goal_is_paused_subscriber = rospy.Subscriber('/move_to_goal/paused', Bool, self._moveToTag)
-
-        self.pub_move_to_tag = rospy.Publisher('move_to_tag_start_driving', Bool, queue_size=10)
+        print("--- init service ---")
+        # --- init service ---
+        self.enable_blob_detection_service = rospy.Service('enable_blob_detection_service', EnableBlobDetection, self._set_blob_detection)
+        self.enable_tag_known_check_service = rospy.Service('enable_tag_known_check_service', EnableTagKnownCheck, self._set_tag_known_check)
 
         print "--- CAMERA READY ---"
         rospy.spin() 
+
+    def _set_blob_detection(self,data):
+        """
+        Enable or disable the Blob detection
+        """
+        self.do_blob_detection = data.enableBlobDetection
+
+    def _set_tag_known_check(self,data):
+        """
+        Enable or disable the Tag Known check
+        """
+        self.do_tag_known_check = data.enableTagKnownCheck
 
     def _update_pose(self, data):
 		"""
@@ -100,18 +117,18 @@ class Camera():
 		return yaw  
         
     def _run(self, image):
-        if self.received_map == True:
+        if self.received_map == True and self.do_blob_detection == True:
             #get frame from robo
             frame = self.bridge.compressed_imgmsg_to_cv2(image, desired_encoding='bgr8') 
             #Save Iamge for H matrix
             #cv2.imwrite('03-03.jpg',frame)
             #raw_input("stop")
 
-            #self._showImage("img2", frame, False)
+            #self._show_image("img2", frame, False)
             #cut the top of the frame to only see 45 cm
             frame = frame[0:720, 0:1280]
             #calculate hsv mask
-            mask = self._calculateMask(frame)
+            mask = self._calculate_mask(frame)
             #detect blob if x y == 0 no blob detected
             (self.blob_x, self.blob_y) = self._find_center(mask, self.minAreaSize)
             #publish
@@ -119,7 +136,7 @@ class Camera():
                 self.blob_msg.blob_detected = True
                 self.blob_msg.blob_x = self.blob_x
                 self.blob_msg.blob_y = self.blob_y
-                self.pub_blob.publish(self.blob_msg)              
+                self.blob_publisher.publish(self.blob_msg)              
                 if self.blob_in_front == False:
                     self.blob_in_front = True
                     #------------------------------------
@@ -131,40 +148,45 @@ class Camera():
                 self.blob_msg.blob_detected = False
                 self.blob_msg.blob_x = 0
                 self.blob_msg.blob_y = 0
-                self.pub_blob.publish(self.blob_msg)
+                self.blob_publisher.publish(self.blob_msg)
             #show image with centroid
-            #self._showImage("img1", mask, True)
+            #self._show_image("img1", mask, True)
         else:
             print "map not running"
 
-    def _moveToTag(self, data):
+    def _move_to_tag(self, data):
         # wenn stop true
         if data.data == True:
-            next_x = self.pose.position.x + math.cos(self.robot_yaw) * 0.20
-            next_y = self.pose.position.y + math.sin(self.robot_yaw) * 0.20
-            robo_x_in_map = int(math.floor((next_x - self.map_offset_x)/self.map_resolution))
-            robo_y_in_map = int(math.floor((next_y - self.map_offset_y)/self.map_resolution))
-            check_service_response = self.tag_manager_check(robo_x_in_map,robo_y_in_map)
-            if check_service_response.tagKnown.data == False:
-                #------------------------------------
-                # send move to Tag
-                #------------------------------------
+            if self.do_tag_known_check == True:
+                next_x = self.pose.position.x + math.cos(self.robot_yaw) * 0.20
+                next_y = self.pose.position.y + math.sin(self.robot_yaw) * 0.20
+                robo_x_in_map = int(math.floor((next_x - self.map_offset_x)/self.map_resolution))
+                robo_y_in_map = int(math.floor((next_y - self.map_offset_y)/self.map_resolution))
+                check_service_response = self.tag_manager_check_service(robo_x_in_map,robo_y_in_map)
+                if check_service_response.tagKnown.data == False:
+                    #------------------------------------
+                    # send move to Tag
+                    #------------------------------------
+                    msg_str = Bool()
+                    msg_str = True
+                    self.move_to_tag_publisher.publish(msg_str)
+                else:
+                    self.stop_move_to_goal_publisher.publish(False) 
+            else:
                 msg_str = Bool()
                 msg_str = True
-                self.pub_move_to_tag.publish(msg_str)
-            else:
-                self.stop_move_to_goal_publisher.publish(False) 
+                self.move_to_tag_publisher.publish(msg_str)
         else: 
             self.stop_move_to_goal_publisher.publish(False)
         
-    def _showImage(self, name, img, centroid):
+    def _show_image(self, name, img, centroid):
         #show images
         if centroid == True:
             cv2.circle(img, (self.blob_x, self.blob_y), 20, self.COLOR, thickness=5, lineType=8, shift=0)
         cv2.imshow(name, img) 
         cv2.waitKey(1)
 
-    def _calculateMask(self, frame):
+    def _calculate_mask(self, frame):
         im = cv2.blur(frame, (3, 3))
         # convert to hsv image
         hsv = cv2.cvtColor(im, cv2.COLOR_BGR2HSV)
@@ -202,30 +224,6 @@ class Camera():
         else:
             #print "not found!"
             return 0, 0
-    
-    def _calculateMapPosOfTag(self, x, y):
-        H = np.array([[0.230489985832561, 3.57275008588951e-05, -146.602459530058],
-                      [0.00589738679860119, 0.120006465885370, 257.753591375280],
-                      [2.75956230239940e-05 , 0.0172475696150036, 1.0]])
-
-        result = H.dot(np.asarray([x, y, 1]))
-
-        x = result[1] / result[2]
-        y = result[0] / result[2]
-
-        _, _, phi = euler_from_quaternion([self.pose.orientation.x, self.pose.orientation.y, self.pose.orientation.z, self.pose.orientation.w])
-
-        T = np.asarray(
-            [
-                [np.cos(phi), -np.sin(phi), self.pose.position.x],
-                [np.sin(phi), np.cos(phi), self.pose.position.y],
-                [0, 0, 1]
-            ]
-        )
-
-        x, y, _ = np.matmul(T, np.asarray([(x/100), (y/100), 1]))
-
-        return [x, y]
 
 if __name__ == '__main__':
     try:
